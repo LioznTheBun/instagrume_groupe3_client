9e9678dd38fc1d143f157f9372258ae1ae167879
LioznTheBun <etienne.pourchon@gmail.com>
2023-11-29 11:54:07 +0100

    Affichage de l'accueil partiel à styliser

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,47 +1,59 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Service\ApiLinker;
 use phpDocumentor\Reflection\Types\Null_;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
 
-class PageController extends AbstractController {
+class PageController extends AbstractController
+{
 
     private $apiLinker;
-  
-    public function __construct(ApiLinker $apiLinker) {
-        $this->apiLinker = $apiLinker;
-     }
 
-    #[Route('/', methods: ['GET'])]
-    public function displayConnexionPage() {
+    public function __construct(ApiLinker $apiLinker)
+    {
+        $this->apiLinker = $apiLinker;
+    }
+
+    #[Route('/login', methods: ['GET'])]
+    public function displayConnexionPage()
+    {
         return $this->render('accueil.html.twig', ['page' => 'accueil', 'role' => NULL]);
     }
 
-    #[Route('/accueil', methods: ['GET'], condition: "service('route_checker').checkUser(request)")]
-    public function displayaccueilPage(Request $request) {
+    #[Route('/', methods: ['GET'])]
+    public function displayaccueilPage(Request $request)
+    {
+        $role = NULL;
         $session = $request->getSession();
         $token = $session->get('token-session');
 
-        $response = $this->apiLinker->getData('/accueil', $token);
-        $accueil = json_decode($response);
+        $posts = $this->apiLinker->getData('/publications', $token);
 
         // récupération du rôle de l'utilisateur courant
-        $jsonUser = $this->apiLinker->getData('/myself', $token);
-        $user = json_decode($jsonUser);
-        $role = 'membre';
-        if(in_array('ROLE_ADMIN', $user->roles)) {
-            $role = 'admin';
+        if ($token) {
+            $jsonUser = $this->apiLinker->getData('/myself', $token);
+            $user = json_decode($jsonUser);
+            $role = 'membre';
+            if (in_array('ROLE_ADMIN', $user->roles)) {
+                $role = 'admin';
+            }
         }
 
-        return $this->render('accueil.html.twig', ['accueil' => $accueil, 'role' => $role, 'page' => 'accueil']);
+        $arrayPosts = json_decode($posts);
+        shuffle($arrayPosts);
+
+        return $this->render('accueil.html.twig', ['role' => $role, 'page' => 'accueil', 'posts' => $arrayPosts]);
     }
 
     #[Route('/users', methods: ['GET'], condition: "service('route_checker').checkAdmin(request)")]
-    public function displayUtilisateursPage(Request $request) {
+    public function displayUtilisateursPage(Request $request)
+    {
         $session = $request->getSession();
         $token = $session->get('token-session');
 
@@ -50,6 +62,4 @@ class PageController extends AbstractController {
 
         return $this->render('users.html.twig', ['users' => $users, 'role' => 'admin']);
     }
-
-	
 }

eef0acbd10cff4e5a8e6448325e28adc8bee9951
LioznTheBun <etienne.pourchon@gmail.com>
2023-12-05 16:57:56 +0100

    Ajout css pour formulaire et amélio connexion/inscription

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -10,37 +10,42 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\JsonConverter;
 use App\Service\ApiLinker;
 
-class ConnexionController extends AbstractController {
+class ConnexionController extends AbstractController
+{
 
     private $jsonConverter;
     private $apiLinker;
 
-    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter) {
+    public function __construct(ApiLinker $apiLinker, JsonConverter $jsonConverter)
+    {
         $this->apiLinker = $apiLinker;
         $this->jsonConverter = $jsonConverter;
     }
 
     #[Route('/login', methods: ['POST'])]
-    public function connexion(Request $request) {
-
+    public function connexion(Request $request)
+    {
+        $responseObject = "";
         $email = $request->request->get('email');
         $password = $request->request->get('password');
 
-        if (!empty($email) && !empty($password)) {
-            $data = $this->jsonConverter->encodeToJson(['email' => $email, 'password' => $password]);
-            $response = $this->apiLinker->postData('/login', $data, null);
-            $responseObject = json_decode($response);
+        $data = $this->jsonConverter->encodeToJson(['email' => $email, 'password' => $password]);
+        $response = $this->apiLinker->postData('/login', $data, null);
+        $responseObject = json_decode($response);
+
+        if (!is_String($responseObject)) {
 
             $session = $request->getSession();
             $session->set('token-session', $responseObject->token);
 
             return $this->redirect('/');
         }
-        return $this->redirect('/login');
+        return $this->render('connexion.html.twig', ['response' => $responseObject, 'role' => NULL, 'page' => 'connexion']);
     }
 
     #[Route('/logout', methods: ['GET'])]
-    public function deconnexion(Request $request) {
+    public function deconnexion(Request $request)
+    {
         $session = $request->getSession();
         $session->remove('token-session');
         $session->clear();
